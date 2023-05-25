b1a00fb61bd669e041547ca5bd852af211e618d0
РомаН <119739400+romausnul@users.noreply.github.com>
2023-05-25 08:56:16 +0300

    Add files via upload

--- article.php
+++ article.php
@@ -9,3 +9,4 @@ $article = articles_get($link, $_GET['id']);
 include("views/article.php");
 
 ?>
+<a href="https://ibb.co/pKPMqyK"><img src="https://i.ibb.co/6tZTQBt/image.png" alt="image" border="0"></a>
